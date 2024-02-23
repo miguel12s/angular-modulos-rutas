a1293a7ff471b4d85c9c7f77761fc66600f16a8f
MIGUEL ANGEL SUAREZ PLATA <mmiguelsuarez@itsa.edu.local>
2024-02-22 20:44:57 -0500

    commit

--- src/app/auth/pages/login/login.component.ts
+++ src/app/auth/pages/login/login.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  public loginForm!:FormGroup 
+  public fb=inject(FormBuilder)
+  
+constructor(){
+  this.loginForm=this.initForm()
+}
+
+initForm():FormGroup{
+  return this.fb.group({
+    email:['',Validators.required,Validators.email],
+    password:['',Validators.required,Validators.minLength(6)]
+  }) 
+}
+
+
 
 }
