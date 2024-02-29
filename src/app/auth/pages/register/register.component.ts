c2057463251583d33d222a25a131c2124804a59f
miguel12s <87570624+miguel12s@users.noreply.github.com>
2024-02-28 23:07:19 -0500

    commit

--- src/app/auth/pages/register/register.component.ts
+++ src/app/auth/pages/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  public fb = inject(FormBuilder)
+  public user!:FormGroup
+  // public service=inject(UserService)
+  public imageSrc!: string 
+  constructor(){
+    this.user=this.initForm()
+  }
 
+  initForm(): FormGroup {
+    return this.fb.group(
+      {
+        nombres: ['', [Validators.required,Validators.minLength(10),Validators.maxLength(30)]],
+        profesion: ['', [Validators.required,Validators.minLength(6),Validators.maxLength(30)]],
+        numero: ['', [Validators.required,Validators.min(3000000000),Validators.max(3250000000)]],
+        correo:['',[Validators.pattern('.*@(unibarranquilla\.edu\.co|itsa\.edu\.co)$')]],
+        password: ['', [Validators.required,Validators.minLength(7),Validators.maxLength(10)]],
+        
+      },
+      
+     
+
+
+    )
+  }
+  onSubmit(){
+   const user:any= this.user.value
+  //  user.telefono=this.service.formatPhone(telefono)
+
+    
+    console.log(user);
+    
+    // this.service.addSuperhero(user)
+  }
 }
